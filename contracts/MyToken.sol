cb685c925b7bf2fda49cf2ec0c64e013947b68b6
SimoneSchiavone <simone.schiavone@gmail.com>
2022-12-19 09:44:56 +0100

    1

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyToken is ERC721 {
-    string topo;
+    string test;
+    uint propr;
     constructor() ERC721("MyToken", "MTK") {}
 }
