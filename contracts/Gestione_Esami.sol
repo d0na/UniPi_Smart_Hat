7a55b34f15fa748c173daac7867de9e427341401
SimoneSchiavone <simone.schiavone@gmail.com>
2022-12-20 22:12:11 +0100

    Contratto Gestione_Esami

--- contracts/Gestione_Esami.sol
+++ contracts/Gestione_Esami.sol
@@ -1,26 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
-contract Gestione_Esami {
+contract Gestione_Esami is Ownable {
 
     enum examState{TO_DO, PASSED, PASSED_WITH_MERIT}
-
-    //Exams state
-    examState RetiDiCalcolatori=examState.PASSED;
-    examState Crittografia=examState.TO_DO;
-    bool public graduated=false;
-
+   
+    /*  (Codice esame => (indirizzo studente => stato esame))
+        Per ogni esame la mappa contiene il relativo stato per ogni studente*/
+    mapping(string => mapping(address => examState)) exams;
+    mapping(address => bool) graduated;
+    string[] planned_exams;
     constructor() {
+        planned_exams.push("274AA");
+        planned_exams.push("245AA");
     }
 
-    function getExamsState(address who) public view returns (examState _exam1,examState _exam2) {
-        if(who == address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)){
-            _exam1=examState.PASSED;
-            _exam2=examState.TO_DO;
-        }else{
-           _exam1=RetiDiCalcolatori;
-            _exam2=Crittografia; 
+    function getExamState(address who,string memory examCode) public view returns (examState) {
+        //Controllo codice esame
+        require(checkExamCode(examCode),"Unknown exam code");
+
+        return exams[examCode][who];
+    }
+
+    function addExamResult(address who, string memory examCode, examState assestment) public onlyOwner {
+        //Controllo valutazioni
+        require(uint(assestment)!=uint(examState.TO_DO),"Not allowed to delete an evaluation");
+        require(uint(assestment)<=2,"Unknown assestment");
+        //Controllo codice esame
+        require(checkExamCode(examCode),"Unknown exam code");
+        //Controllo se lo studente ha già passato l'esame (no modifiche di voto)
+        require(exams[examCode][who]==examState.TO_DO,"The student has already passed this exam");
+
+        exams[examCode][who]=assestment;
+    }
+
+    function publishGraduation(address who) public onlyOwner {
+        require(!graduated[who],"The student is already graduated");
+        for(uint i=0;i<planned_exams.length;i++){
+            if(exams[planned_exams[i]][who]==examState.TO_DO)
+                revert("The student has not passed all the exams required for the degree");
         }
-        
+        graduated[who]=true;
+    }
+
+    function getMyExamState(string memory examCode) public view returns (examState){
+        return getExamState(msg.sender, examCode);
+    }
+    
+    function addExam(string memory examCode) public onlyOwner{
+        require(!checkExamCode(examCode),"Exam already added");
+        planned_exams.push(examCode);
+    }
+
+    function checkExamCode(string memory examCode) internal view returns (bool){
+        for(uint i=0;i<planned_exams.length;i++){
+            if(keccak256(abi.encodePacked(planned_exams[i])) == keccak256(abi.encodePacked(examCode))){
+                return true;
+            } 
+        }
+        return false;
+    }
+
+    function isGraduated(address who) public view returns (bool){
+        return graduated[who];
     }
 }
--- contracts/Smart_Hat.sol
+++ contracts/Smart_Hat.sol
@@ -4,14 +4,13 @@ import "./Gestione_Esami.sol";
 
 contract Smart_Hat {
     Gestione_Esami private manager;
-    enum examState{TO_DO, PASSED, PASSED_WITH_MERIT} 
 
     constructor() {
         manager = new Gestione_Esami();
     }
 
-    function crea_cappellino() public {
-        (examState _e1,examState _e2)=manager.getExamsState(msg.sender);
+    function crea_cappellino() public view returns(Gestione_Esami.examState _e1, Gestione_Esami.examState _e2) {
+        (_e1, _e2)=manager.getExamsState(msg.sender);
     }
 
     function aggiungi_spilla() public {
